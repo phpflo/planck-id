e9dd5268090dc7ac0562aaec68fa95871bb918fd
aretecode <aretecode@gmail.com>
2015-09-16 10:04:33 -0700

    Remove composer.lock, add to ReadExternalFile

--- src/Utilities/ReadExternalFile.php
+++ src/Utilities/ReadExternalFile.php
@@ -1,3 +1,17 @@
 <?php
 
-class ReadExternalFile {}
+namespace PlanckId\Utilities;
+
+class ReadExternalFile {
+    protected $fileName;
+    protected $content;
+
+    public function setFileName($fileName) {
+        $this->fileName($fileName);
+    }
+
+    // could also pass in the $fileName to the __invoke 
+    public function __invoke() {
+        $this->content = file_get_contents($this->fileName);
+    }
+}
