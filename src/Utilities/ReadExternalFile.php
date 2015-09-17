c69135882dd9e1676eee2088d4225c8eab5f7a01
aretecode <aretecode@gmail.com>
2015-09-17 08:52:35 -0700

    Remove comments.

--- src/Utilities/ReadExternalFile.php
+++ src/Utilities/ReadExternalFile.php
@@ -17,20 +17,3 @@ class ReadExternalFile extends InvokableFloComponent {
         $this->content = file_get_contents($this->fileName);
     }
 }
-
-
-Could pass it into ReadExternalFile
-
-Could call multiple ports to pass on the $content
-
-So when it gets passed on where does it go?
-
-
-
-
-1) WHERE DOES IT GO
-2) WHERE DOES IT GET REPLACED? > CAN IT LOOP BACK ON ITSELF AND CALL THE INITIAL FLO WITH AN ARRAY IN PORT (NOT INITIAL, BUT THE ONE WHERE IT REPLACES?)?
-3) WHERE DOES IT SAVE/OUTPUT? 
-   A) DOES IT TRY TO REPLACE THE SAME FILE
-   B) DOES IT CREATE A LOCAL COPY?
-   C) ALL OF THE ABOVE, SEE IF IT CAN REPLACE - IF NOT, CREATE A LOCAL COPY - IF NOT, OUTPUT IN TEXT.
